aa193a27b924fb512c85d83d2b94f82100ba23c3
Lucas Melo <lucas.melo044@gmail.com>
2022-05-01 20:53:08 -0300

    add section cards && estilizacao mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const buttonMenu = document.querySelector('.header__button');
 const buttonMenuImages = ['./assets/img/svg/menu.svg', './assets/img/svg/close.svg']
+const cards = document.querySelectorAll('.card');
+
 
 buttonMenu.addEventListener('click', () => {
   buttonMenu.style.backgroundImage = `url(${buttonMenuImages[1]})`;
@@ -7,3 +9,18 @@ buttonMenu.addEventListener('click', () => {
 
   document.querySelector('.header__menu').classList.toggle('header__menu--open');
 })
+
+for (let card of cards) {
+  let button = card.querySelector('.card__button');
+
+  card.addEventListener('mouseover', () => {
+    card.classList.add('card--horver');
+    button.classList.add('card__button--horver');
+  })
+
+  card.addEventListener('mouseout', () => {
+    card.classList.remove('card--horver');
+    button.classList.remove('card__button--horver');
+  })
+
+}
